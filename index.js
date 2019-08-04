7137a2c7cfb3595419fd5c66a9745e27c27d8767
Aaron Matson <31553895+aaronmatson1@users.noreply.github.com>
2019-08-03 22:24:31 -0600

    Added Node_modules folder

--- index.js
+++ index.js
@@ -1,118 +1,326 @@
-// This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
-// Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
-// session persistence, api calls, and more.
-const Alexa = require('ask-sdk-core');
+/* This code has been generated from your interaction model by skillinator.io
 
-const LaunchRequestHandler = {
-    canHandle(handlerInput) {
-        return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+/* eslint-disable  func-names */
+/* eslint quote-props: ["error", "consistent"]*/
+
+// There are three sections, Text Strings, Skill Code, and Helper Function(s).
+// You can copy and paste the contents as the code for a new Lambda function, using the alexa-skill-kit-sdk-factskill template.
+// This code includes helper functions for compatibility with versions of the SDK prior to 1.0.9, which includes the dialog directives.
+
+
+
+ // 1. Text strings =====================================================================================================
+ //    Modify these strings and messages to change the behavior of your Lambda function
+
+
+let speechOutput;
+let reprompt;
+let welcomeOutput = "Welcome to Guitar Tuner! What note would you like me to play?";
+let welcomeReprompt = "what string do you want to ";
+// 2. Skill Code =======================================================================================================
+"use strict";
+const Alexa = require('alexa-sdk');
+const APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
+speechOutput = '';
+const handlers = {
+	'LaunchRequest': function () {
+		this.emit(':ask', welcomeOutput, welcomeReprompt);
+	},
+	'AMAZON.HelpIntent': function () {
+		speechOutput = 'Placeholder response for AMAZON.HelpIntent.';
+		reprompt = '';
+		this.emit(':ask', speechOutput, reprompt);
+	},
+   'AMAZON.CancelIntent': function () {
+		speechOutput = 'Placeholder response for AMAZON.CancelIntent';
+		this.emit(':tell', speechOutput);
+	},
+   'AMAZON.StopIntent': function () {
+		speechOutput = 'Placeholder response for AMAZON.StopIntent.';
+		this.emit(':tell', speechOutput);
+   },
+   'SessionEndedRequest': function () {
+		speechOutput = '';
+		//this.emit(':saveState', true);//uncomment to save attributes to db on session end
+		this.emit(':tell', speechOutput);
+   },
+
+	'AMAZON.NavigateHomeIntent': function () {
+		speechOutput = '';
+
+		//any intent slot variables are listed here for convenience
+
+
+		//Your custom intent handling goes here
+		speechOutput = "Here is a " + noteSlot + '<audio src="https://s3.amazonaws.com/guitar-tuner-alexa-ajm/low-e.mp3"';
+		this.emit(":ask", speechOutput, speechOutput);
     },
-    handle(handlerInput) {
-        const speechText = 'Welcome, you can say Hello or Help. Which would you like to try?';
-        return handlerInput.responseBuilder
-            .speak(speechText)
-            .reprompt(speechText)
-            .getResponse();
+	'PlayNoteIntent': function () {
+		speechOutput = '';
+		speechReprompt = '';
+
+		//any intent slot variables are listed here for convenience
+
+
+		let noteSlot = resolveCanonical(this.event.request.intent.slots.note);
+		console.log('User requested note: ' + noteSlot);
+
+		let pitchSlot = resolveCanonical(this.event.request.intent.slots.pitch);
+		console.log('User requested pitche: ' + pitchSlot);
+
+		var notes = {
+		    'a' :{
+		        'low':'https://s3.amazonaws.com/guitar-tuner-alexa-ajm/5th_String_A_Converted.mp3',
+		        'high':'https://s3.amazonaws.com/guitar-tuner-alexa-ajm/5th_String_A_Converted.mp3'
+		    },
+
+		    'b' :{
+		        'low':'https://s3.amazonaws.com/guitar-tuner-alexa-ajm/2nd_String_B_Converted.mp3',
+		        'high':'https://s3.amazonaws.com/guitar-tuner-alexa-ajm/2nd_String_B_Converted.mp3'
+		    },
+
+		    'd' :{
+		        'low':'https://s3.amazonaws.com/guitar-tuner-alexa-ajm/4th_String_D_Converted.mp3',
+		        'high':'https://s3.amazonaws.com/guitar-tuner-alexa-ajm/4th_String_D_Converted.mp3'
+		    },
+
+		    'e' :{
+		        'low':'https://s3.amazonaws.com/guitar-tuner-alexa-ajm/low-e_Converted.mp3',
+		        'high':'https://s3.amazonaws.com/guitar-tuner-alexa-ajm/high-e_Converted.mp3'
+		    },
+
+		    'g' :{
+		        'low':'https://s3.amazonaws.com/guitar-tuner-alexa-ajm/3rd_String_g_Converted.mp3',
+		        'high':'https://s3.amazonaws.com/guitar-tuner-alexa-ajm/3rd_String_g_Converted.mp3'
+		    }
+		}
+
+		var pitches = [
+		        'low',
+		        'high'
+		    ];
+
+		var note = noteSlot.toLowerCase();
+
+		if(noteSlot && notes[note]) {
+		    var audio = '';
+		    var pitch = 'low'; //default pitch
+
+		    if (pitchSlot && pitch.indexOf(pitchSlot) > -1) {
+		        pitch = pitchSlot;
+		    }
+
+		    var audioSrc = notes[note][pitch];
+
+		    speechOutput = 'Here is your note: <audio src="' + audioSrc + '" />';
+		    speechReprompt = "Would you like to hear it again?";
+
+		} else {
+		    speechOutput = 'Sorry, the note you asked for is not supported yet.';
+		    speechReprompt = "I support notes A, B, D, low E, high E, and G";
+		}
+
+		this.emit(":ask", speechOutput, speechReprompt);
+
+		//Your custom intent handling goes here
+		speechOutput = "Ok, I will play a " + noteSlot + " with Pitch " + pitchSlot;
+		this.emit(":ask", speechOutput, speechOutput);
+    },
+	'Unhandled': function () {
+        speechOutput = "I didn't quite understand what you wanted.  Do you want to try something else?";
+        this.emit(':ask', speechOutput, "Would you l ike to hear this again?");
     }
 };
-const HelloWorldIntentHandler = {
-    canHandle(handlerInput) {
-        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-            && handlerInput.requestEnvelope.request.intent.name === 'HelloWorldIntent';
-    },
-    handle(handlerInput) {
-        const speechText = 'Hello World!';
-        return handlerInput.responseBuilder
-            .speak(speechText)
-            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
-            .getResponse();
-    }
-};
-const HelpIntentHandler = {
-    canHandle(handlerInput) {
-        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-            && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
-    },
-    handle(handlerInput) {
-        const speechText = 'You can say hello to me! How can I help?';
 
-        return handlerInput.responseBuilder
-            .speak(speechText)
-            .reprompt(speechText)
-            .getResponse();
-    }
-};
-const CancelAndStopIntentHandler = {
-    canHandle(handlerInput) {
-        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-            && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
-                || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
-    },
-    handle(handlerInput) {
-        const speechText = 'Goodbye!';
-        return handlerInput.responseBuilder
-            .speak(speechText)
-            .getResponse();
-    }
-};
-const SessionEndedRequestHandler = {
-    canHandle(handlerInput) {
-        return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
-    },
-    handle(handlerInput) {
-        // Any cleanup logic goes here.
-        return handlerInput.responseBuilder.getResponse();
-    }
+exports.handler = (event, context) => {
+    const alexa = Alexa.handler(event, context);
+    alexa.appId = APP_ID;
+    // To enable string internationalization (i18n) features, set a resources object.
+    //alexa.resources = languageStrings;
+    alexa.registerHandlers(handlers);
+	//alexa.dynamoDBTableName = 'DYNAMODB_TABLE_NAME'; //uncomment this line to save attributes to DB
+    alexa.execute();
 };
 
-// The intent reflector is used for interaction model testing and debugging.
-// It will simply repeat the intent the user said. You can create custom handlers
-// for your intents by defining them above, then also adding them to the request
-// handler chain below.
-const IntentReflectorHandler = {
-    canHandle(handlerInput) {
-        return handlerInput.requestEnvelope.request.type === 'IntentRequest';
-    },
-    handle(handlerInput) {
-        const intentName = handlerInput.requestEnvelope.request.intent.name;
-        const speechText = `You just triggered ${intentName}`;
+//    END of Intent Handlers {} ========================================================================================
+// 3. Helper Function  =================================================================================================
 
-        return handlerInput.responseBuilder
-            .speak(speechText)
-            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
-            .getResponse();
-    }
+function resolveCanonical(slot){
+	//this function looks at the entity resolution part of request and returns the slot value if a synonyms is provided
+	let canonical;
+    try{
+		canonical = slot.resolutions.resolutionsPerAuthority[0].values[0].value.name;
+	}catch(err){
+	    console.log(err.message);
+	    canonical = slot.value;
+	};
+	return canonical;
 };
 
-// Generic error handling to capture any syntax or routing errors. If you receive an error
-// stating the request handler chain is not found, you have not implemented a handler for
-// the intent being invoked or included it in the skill builder below.
-const ErrorHandler = {
-    canHandle() {
-        return true;
-    },
-    handle(handlerInput, error) {
-        console.log(`~~~~ Error handled: ${error.message}`);
-        const speechText = `Sorry, I couldn't understand what you said. Please try again.`;
+function delegateSlotCollection(){
+  console.log("in delegateSlotCollection");
+  console.log("current dialogState: "+this.event.request.dialogState);
+    if (this.event.request.dialogState === "STARTED") {
+      console.log("in Beginning");
+	  let updatedIntent= null;
+	  // updatedIntent=this.event.request.intent;
+      //optionally pre-fill slots: update the intent object with slot values for which
+      //you have defaults, then return Dialog.Delegate with this updated intent
+      // in the updatedIntent property
+      //this.emit(":delegate", updatedIntent); //uncomment this is using ASK SDK 1.0.9 or newer
 
-        return handlerInput.responseBuilder
-            .speak(speechText)
-            .reprompt(speechText)
-            .getResponse();
+	  //this code is necessary if using ASK SDK versions prior to 1.0.9
+	  if(this.isOverridden()) {
+			return;
+		}
+		this.handler.response = buildSpeechletResponse({
+			sessionAttributes: this.attributes,
+			directives: getDialogDirectives('Dialog.Delegate', updatedIntent, null),
+			shouldEndSession: false
+		});
+		this.emit(':responseReady', updatedIntent);
+
+    } else if (this.event.request.dialogState !== "COMPLETED") {
+      console.log("in not completed");
+      // return a Dialog.Delegate directive with no updatedIntent property.
+      //this.emit(":delegate"); //uncomment this is using ASK SDK 1.0.9 or newer
+
+	  //this code necessary is using ASK SDK versions prior to 1.0.9
+		if(this.isOverridden()) {
+			return;
+		}
+		this.handler.response = buildSpeechletResponse({
+			sessionAttributes: this.attributes,
+			directives: getDialogDirectives('Dialog.Delegate', null, null),
+			shouldEndSession: false
+		});
+		this.emit(':responseReady');
+
+    } else {
+      console.log("in completed");
+      console.log("returning: "+ JSON.stringify(this.event.request.intent));
+      // Dialog is now complete and all required slots should be filled,
+      // so call your normal intent handler.
+      return this.event.request.intent;
     }
-};
+}
 
-// This handler acts as the entry point for your skill, routing all request and response
-// payloads to the handlers above. Make sure any new handlers or interceptors you've
-// defined are included below. The order matters - they're processed top to bottom.
-exports.handler = Alexa.SkillBuilders.custom()
-    .addRequestHandlers(
-        LaunchRequestHandler,
-        HelloWorldIntentHandler,
-        HelpIntentHandler,
-        CancelAndStopIntentHandler,
-        SessionEndedRequestHandler,
-        IntentReflectorHandler) // make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
-    .addErrorHandlers(
-        ErrorHandler)
-    .lambda();
+
+function randomPhrase(array) {
+    // the argument is an array [] of words or phrases
+    let i = 0;
+    i = Math.floor(Math.random() * array.length);
+    return(array[i]);
+}
+function isSlotValid(request, slotName){
+        let slot = request.intent.slots[slotName];
+        //console.log("request = "+JSON.stringify(request)); //uncomment if you want to see the request
+        let slotValue;
+
+        //if we have a slot, get the text and store it into speechOutput
+        if (slot && slot.value) {
+            //we have a value in the slot
+            slotValue = slot.value.toLowerCase();
+            return slotValue;
+        } else {
+            //we didn't get a value in the slot.
+            return false;
+        }
+}
+
+//These functions are here to allow dialog directives to work with SDK versions prior to 1.0.9
+//will be removed once Lambda templates are updated with the latest SDK
+
+function createSpeechObject(optionsParam) {
+    if (optionsParam && optionsParam.type === 'SSML') {
+        return {
+            type: optionsParam.type,
+            ssml: optionsParam['speech']
+        };
+    } else {
+        return {
+            type: optionsParam.type || 'PlainText',
+            text: optionsParam['speech'] || optionsParam
+        };
+    }
+}
+
+function buildSpeechletResponse(options) {
+    let alexaResponse = {
+        shouldEndSession: options.shouldEndSession
+    };
+
+    if (options.output) {
+        alexaResponse.outputSpeech = createSpeechObject(options.output);
+    }
+
+    if (options.reprompt) {
+        alexaResponse.reprompt = {
+            outputSpeech: createSpeechObject(options.reprompt)
+        };
+    }
+
+    if (options.directives) {
+        alexaResponse.directives = options.directives;
+    }
+
+    if (options.cardTitle && options.cardContent) {
+        alexaResponse.card = {
+            type: 'Simple',
+            title: options.cardTitle,
+            content: options.cardContent
+        };
+
+        if(options.cardImage && (options.cardImage.smallImageUrl || options.cardImage.largeImageUrl)) {
+            alexaResponse.card.type = 'Standard';
+            alexaResponse.card['image'] = {};
+
+            delete alexaResponse.card.content;
+            alexaResponse.card.text = options.cardContent;
+
+            if(options.cardImage.smallImageUrl) {
+                alexaResponse.card.image['smallImageUrl'] = options.cardImage.smallImageUrl;
+            }
+
+            if(options.cardImage.largeImageUrl) {
+                alexaResponse.card.image['largeImageUrl'] = options.cardImage.largeImageUrl;
+            }
+        }
+    } else if (options.cardType === 'LinkAccount') {
+        alexaResponse.card = {
+            type: 'LinkAccount'
+        };
+    } else if (options.cardType === 'AskForPermissionsConsent') {
+        alexaResponse.card = {
+            type: 'AskForPermissionsConsent',
+            permissions: options.permissions
+        };
+    }
+
+    let returnResult = {
+        version: '1.0',
+        response: alexaResponse
+    };
+
+    if (options.sessionAttributes) {
+        returnResult.sessionAttributes = options.sessionAttributes;
+    }
+    return returnResult;
+}
+
+function getDialogDirectives(dialogType, updatedIntent, slotName) {
+    let directive = {
+        type: dialogType
+    };
+
+    if (dialogType === 'Dialog.ElicitSlot') {
+        directive.slotToElicit = slotName;
+    } else if (dialogType === 'Dialog.ConfirmSlot') {
+        directive.slotToConfirm = slotName;
+    }
+
+    if (updatedIntent) {
+        directive.updatedIntent = updatedIntent;
+    }
+    return [directive];
+}
